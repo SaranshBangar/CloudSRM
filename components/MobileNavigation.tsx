37680fc429d24c7c8e9345715aae9a77279d3c1b
Saransh Bangar <saranshbangad@gmail.com>
2025-03-17 19:02:48 +0530

    mobile nav complete

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,7 +1,79 @@
-import React from "react";
+"use client";
 
-const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+import React, { useEffect, useState } from "react";
+
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { getAvatarUrl } from "@/lib/actions/user.actions";
+import { Separator } from "./ui/separator";
+import { sidebarNavItems } from "@/constants";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import FileUploader from "./FileUploader";
+import { Button } from "./ui/button";
+
+const MobileNavigation = ({ ownerId, accountId, fullName, email }: { ownerId: string; accountId: string; fullName: string; email: string }) => {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  const [avatarUrl, setAvatarUrl] = useState("/assets/images/default-avatar.png");
+
+  useEffect(() => {
+    const fetchAvatarUrl = async () => {
+      try {
+        const url = await getAvatarUrl(email);
+        setAvatarUrl(url);
+      } catch (error) {
+        console.error("Error fetching avatar URL:", error);
+      }
+    };
+
+    fetchAvatarUrl();
+  }, [email]);
+
+  return (
+    <header className="mobile-header">
+      <Image src="/favicon.ico" alt="logo" width={60} height={60} className="h-auto" />
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image src="/assets/icons/menu.svg" alt="search" width={30} height={30} />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+          <SheetTitle>
+            <div className="header-user">
+              <Image src={avatarUrl} alt="avatar" width={44} height={44} className="header-user-avatar" />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+            </div>
+            <Separator className="mb-4 bg-light-200/20" />
+          </SheetTitle>
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {sidebarNavItems.map(({ url, name, icon }) => (
+                <Link href={url} key={name} className="lg:w-full">
+                  <li className={cn("mobile-nav-item", pathname === url && "shad-active")}>
+                    <Image src={icon} alt={name} width={24} height={24} className={cn("nav-icon", pathname === url && "nav-icon-active")} />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="my-5 bg-light-200/20" />
+          <div className="flex flex-col justify-between gap-5 pb-5">
+            <FileUploader />
+            <Button type="submit" className="mobile-sign-out-button" onClick={() => {}}>
+              <Image src="/assets/icons/logout.svg" alt="L=logout" width={24} height={24} />
+              <p>Logout</p>
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
 
 export default MobileNavigation;
