99a3080032e453eae42ddaccb3bf3c4e0de90070
Saransh Bangar <saranshbangad@gmail.com>
2025-04-13 08:33:04 +0530

    Add Google Analytics tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const noto_sans = Noto_Sans({
@@ -24,7 +25,19 @@ export default function RootLayout({
         <meta name="google-site-verification" content="qF71edahFIYLNHvFM63PAKUWBDzbvGg5Dk4j44IM_Eg" />
         <meta name="google-adsense-account" content="ca-pub-4851546257120071" />
       </head>
-      <body className={`${noto_sans.variable} font-noto_sans antialiased`}>{children}</body>
+      <body className={`${noto_sans.variable} font-noto_sans antialiased`}>
+        {children}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-1D7B8H67PV" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-1D7B8H67PV');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
